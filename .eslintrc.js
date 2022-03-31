854496e5954ead8be75dcc93069cb81b8a47ede0
suhong <67825309+tnghd5761@users.noreply.github.com>
2022-03-31 16:23:47 +0900

    install dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+		"plugin:prettier/recommand"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "@typescript-eslint",
+		"prettier"
+    ],
+    "rules": {
+    }
+}
