822747b40f7b1a722e20ccd14effceb68b0fbfc8
Donghyeong Kang <dh@uos.ac.kr>
2022-05-04 01:01:14 +0900

    UPDATE Review: add thread

--- components/DiscussionDetail/Review.tsx
+++ components/DiscussionDetail/Review.tsx
@@ -1,30 +1,22 @@
 import { DiffEditor } from '@monaco-editor/react'
 import { useEffect, useRef, useState } from 'react'
-import { useQuery } from 'react-query'
 import { ReviewResponse } from '../../api/Review'
+import ThreadList from './ThreadList'
 
 type Props = {
 	review: ReviewResponse
 }
 
 const ReviewDetail: React.FC<Props> = ({ review }) => {
-	const [isToggled, setToggled] = useState<boolean>(false)
+	const [isInit, setInit] = useState<boolean>(false)
 	const [selectedDiscussionCode, setSelectedDiscussionCode] =
 		useState<number>(0)
-	const [selectedCodeBefore, setSelectedCodeBefore] = useState<any>('')
-	const [selectedCodeAfter, setSelectedCodeAfter] = useState<any>('')
-	const { data, error, isLoading } = useQuery(
-		['reviewDetail', review.id],
-		() => {
-			return null
-		}
-	)
-
 	const diffEditorRef = useRef<any>(null)
 	const monacoRef = useRef<any>(null)
 	const handleEditorDidMount = (editor: any, monaco: any) => {
 		diffEditorRef.current = editor
 		monacoRef.current = monaco
+		setInit(true)
 	}
 
 	const handleClickDiscussionCode = (idx: number) => {
@@ -32,25 +24,26 @@ const ReviewDetail: React.FC<Props> = ({ review }) => {
 	}
 
 	useEffect(() => {
+		if (!isInit) return
 		if (!monacoRef.current) return
+		if (!review.commentDiffList) return
 		const editor = diffEditorRef.current
 		const monaco = monacoRef.current
-		const selectedReview = review.commentDiffList[selectedDiscussionCode]
+		const selectedReview = isCommentReview(review)
+			? review.commentDiffList[selectedDiscussionCode]
+			: review.liveDiffList[selectedDiscussionCode]
 		const originalModel = monaco.editor.createModel(
-			'just some text\nabcz\nzzzzefgh\nSome more text\nThis line is removed on the left.' //selectedReview.discussionCode.content
+			selectedReview.discussionCode.content
 		)
-		const modifiedModel = monaco.editor.createModel(
-			'This line is removed on the right.\njust some text\nabcd\nefgh\nSome more text'
-		) //selectedReview.codeAfter)
+		const modifiedModel = monaco.editor.createModel(selectedReview.codeAfter)
 		editor.setModel({
 			original: originalModel,
 			modified: modifiedModel
 		})
-	}, [review.commentDiffList, selectedDiscussionCode])
-	// toggled , then hide the review detail
-	// if not toggled, get the review thread & detail
+	}, [review, review.commentDiffList, selectedDiscussionCode, isInit])
+
 	return (
-		<div className="flex-col min-h-[36rem]">
+		<div className="flex-col min-h-[36rem] border-b-2 border-gray-600">
 			<div className="review_header">
 				<div className="avatar placeholder flex items-center my-4">
 					<div className="bg-neutral-focus ring text-neutral-content rounded-full w-8">
@@ -61,25 +54,31 @@ const ReviewDetail: React.FC<Props> = ({ review }) => {
 			</div>
 			<div className="review_code_list flex flex-row">
 				<div className="discussion_code_list basis-1/6">
-					<div className="alert alert-info shadow-lg rounded-none">목록</div>
+					<div className="p-4 border-2 rounded-tl-2xl border-gray-600">
+						목록
+					</div>
 					<div className="discussion_codes">
-						{review.commentDiffList.map((commentDiff, idx) => {
-							return (
-								<div
-									key={idx}
-									className={`pl-4 py-4 cursor-pointer ${
-										idx === selectedDiscussionCode ? 'bg-primary' : ''
-									}`}
-									onClick={() => handleClickDiscussionCode(idx)}
-								>
-									{commentDiff.discussionCode.filename}
-								</div>
-							)
-						})}
+						{[...review.commentDiffList, ...review.liveDiffList].map(
+							(commentDiff, idx) => {
+								return (
+									<div
+										key={idx}
+										className={`pl-4 py-4 cursor-pointer ${
+											idx === selectedDiscussionCode
+												? 'bg-primary-focus'
+												: 'hover:bg-neutral'
+										}`}
+										onClick={() => handleClickDiscussionCode(idx)}
+									>
+										{commentDiff.discussionCode.filename}
+									</div>
+								)
+							}
+						)}
 					</div>
 				</div>
 				<div className="review_code_diff basis-3/6">
-					<div className="alert alert-info shadow-lg rounded-none">코드</div>
+					<div className="p-4 border-2 border-gray-600">코드</div>
 					<div className="code_editor">
 						<DiffEditor
 							height={'30rem'}
@@ -94,14 +93,18 @@ const ReviewDetail: React.FC<Props> = ({ review }) => {
 					</div>
 				</div>
 				<div className="review_code_comment basis-2/6">
-					<div className="alert alert-info shadow-lg rounded-none">코멘트</div>
+					<div className="p-4 border-2 rounded-tr-2xl border-gray-600">
+						코멘트
+					</div>
 					<div className="code_comment p-4">
 						{isCommentReview(review) &&
 							review.commentDiffList[selectedDiscussionCode].comment}
 					</div>
 				</div>
 			</div>
-			<div className="review_thread"></div>
+			<div className="review_thread mt-4">
+				<ThreadList review={review} />
+			</div>
 		</div>
 	)
 }
