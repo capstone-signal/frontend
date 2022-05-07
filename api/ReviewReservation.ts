dcf339e0fd944b321074e2149216828ba29cb096
Donghyeong Kang <dh@uos.ac.kr>
2022-05-07 14:18:37 +0900

    UPDATE reservation api spec

--- api/ReviewReservation.ts
+++ api/ReviewReservation.ts
@@ -1,19 +1,21 @@
 import { CommonResponse, get, post } from './common'
 import { DiscussionResponse } from './Discussion'
+import { UserResponse } from './User'
 
 export type CreateReviewReservationRequest = {
 	/** @format int64 */
 	discussionId: number
 
 	/** @format date-time */
-	reviewDate: Date
+	reviewStartDateTime: Date
 }
 
 export type ReviewReservationResponse = {
 	discussion?: DiscussionResponse
+	reviewer?: UserResponse
 	id: number
 	isdone: boolean
-	reviewDate: Date
+	reviewStartDateTime: Date
 	revieweeParticipated: boolean
 	reviewerParticipated: boolean
 } & CommonResponse
