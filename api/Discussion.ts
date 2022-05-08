921b9c81ba0c958924af02b12ad141afc4eb707d
Donghyeong Kang <dh@uos.ac.kr>
2022-05-08 19:37:48 +0900

    FIX discussion api: language, title 추가

--- api/Discussion.ts
+++ api/Discussion.ts
@@ -13,6 +13,7 @@ export enum DiscussionState {
 export type DirectCode = {
 	content: string
 	filename: string
+	language: string
 }
 
 export type DiscussionBox = {
@@ -29,6 +30,7 @@ type CreateDiscussionRequest = {
 	discussionType: 'PR' | 'COMMIT' | 'DIRECT'
 	question: string
 	tagIds: number[]
+	title: string
 	usePriority: boolean
 	liveReviewRequired: boolean
 	liveReviewAvailableTimes?: {
@@ -56,6 +58,6 @@ export async function createDiscussion(
 	data: CreateDiscussionRequest
 ): Promise<DiscussionResponse> {
 	// 호출하는 쪽에서 data 검증 필요
-	const response = await post<DiscussionResponse>('/discussion', data)
+	const response = await post<DiscussionResponse>('/discussion/', data)
 	return response
 }
