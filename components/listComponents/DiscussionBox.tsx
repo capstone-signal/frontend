ef0fcf46e2cb73b929f4d648dabb727b6cdd1062
suhong <67825309+tnghd5761@users.noreply.github.com>
2022-05-23 20:33:36 +0900

    fix: DiscussionBox UI 수정

--- components/listComponents/DiscussionBox.tsx
+++ components/listComponents/DiscussionBox.tsx
@@ -1,4 +1,6 @@
 import { DiscussionResponse } from '../../api/Discussion'
+import { DiscussionState } from '../../api/Discussion'
+import dayjs from 'dayjs'
 
 type Props = {
 	discussion: DiscussionResponse
@@ -6,15 +8,35 @@ type Props = {
 
 const DiscussionBox: React.FunctionComponent<Props> = ({ discussion }) => {
 	return (
-		<div className="w-full bg-red-200 rounded-xl h-[7rem] p-3 mb-4">
+		<div className="w-full bg-red-200 rounded-xl min-h-[7rem] p-3 mb-4 flex flex-col justify-between overflow-y-visible">
 			<div className="flex flex-row justify-between">
-				<div className="font-bold">{discussion.title}</div>
-				<div className="w-">{discussion.user.name}</div>
+				<span className="font-bold w-[80%] min-h-[4rem]">
+					{discussion.title}
+				</span>
+				<div className="flex flex-col items-end">
+					<span className="font-semibold mb-1">{discussion.user.name}</span>
+					<span className="text-sm mb-1">
+						{dayjs(discussion.createdAt).format('YYYY/MM/DD hh:mm A')}
+					</span>
+					{discussion.state === DiscussionState.NOT_REVIEWED && (
+						<div>Not Reviewed</div>
+					)}
+					{discussion.state === DiscussionState.REVIEWING && (
+						<div>Reviewing</div>
+					)}
+					{discussion.state === DiscussionState.COMPLETED && (
+						<div>Completed</div>
+					)}
+				</div>
 			</div>
-			<div>{discussion.question}</div>
-			<div className="flex flex-row">
+			<div className="flex flex-row overflow-x-auto overflow-y-hidden scroll">
 				{discussion.tags.map((tag, index) => (
-					<div key={index}>{tag.name}</div>
+					<div
+						className="btn btn-sm btn-disabled bg-white rounded-3xl text-sm text-black mr-2 min-w-[2.5rem] border-2 border-solid border-gray-300"
+						key={index}
+					>
+						{tag.name}
+					</div>
 				))}
 			</div>
 		</div>
